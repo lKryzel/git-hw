6d2234ca66d3fb4a344973d250ef304f0445f231
Nikita Kachalov <kachalov1015@gmail.com>
2024-10-13 21:56:32 +0300

    My commit

---
+++ Hello.pas
@@ -0,0 +1,11 @@
+﻿{Измените код в файле hello.pas, чтобы программа спрашивала у пользователя язык,
+на котором он говорит и выводила приветствие на этом языке.}
+begin
+  var language := ReadString('На каком языке вы говорите?:');
+  case language of
+    'English': Println('Hello');
+    'Spanish': Println('Hola');
+    'Chenese': Println('你好！');
+    'Russian': Println('Привет');
+  end;
+end.
